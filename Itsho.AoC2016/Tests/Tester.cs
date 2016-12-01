a03022df0608b77269834dc4917b4219cc4431d9
itsho <itsho.itsho@gmail.com>
2016-12-01 21:17:27 +0200

    * added NUint nuget as tester package

--- Itsho.AoC2016/Tests/Tester.cs
+++ Itsho.AoC2016/Tests/Tester.cs
@@ -1,5 +1,4 @@
 ﻿using Itsho.AoC2016.Solutions;
-using System.Diagnostics;
 
 namespace Itsho.AoC2016.Tests
 {
@@ -9,29 +8,32 @@ namespace Itsho.AoC2016.Tests
         {
             int? intPart2;
             var intPart1 = Day01Solution.GetDistanceFromStartingPoint("R5, L5, R5, R3", out intPart2);
-            Debug.Assert(intPart1 == 12);
+            NUnit.Framework.Assert.AreEqual(12, intPart1);
 
             intPart1 = Day01Solution.GetDistanceFromStartingPoint("R5, R5, R5, R5", out intPart2);
-            Debug.Assert(intPart1 == 0);
+            NUnit.Framework.Assert.AreEqual(0, intPart1);
 
             intPart1 = Day01Solution.GetDistanceFromStartingPoint("R5, R5, R5, R5, R5, R5, R5, R5", out intPart2);
-            Debug.Assert(intPart1 == 0);
+            NUnit.Framework.Assert.AreEqual(0, intPart1);
 
             intPart1 = Day01Solution.GetDistanceFromStartingPoint("R5, R5, L5, R5, R5, L5, L5, L5", out intPart2);
-            Debug.Assert(intPart1 == 20);
+            NUnit.Framework.Assert.AreEqual(20, intPart1);
         }
 
         public static void TestDay1Part2()
         {
             int? intPart2;
             var intPart1 = Day01Solution.GetDistanceFromStartingPoint("R8, R4, R4, R8", out intPart2);
-            Debug.Assert(intPart1 == 8 && intPart2 == 4);
+            NUnit.Framework.Assert.AreEqual(8, intPart1);
+            NUnit.Framework.Assert.AreEqual(4, intPart2);
 
             intPart1 = Day01Solution.GetDistanceFromStartingPoint("R1, R1, R2, R2, R3, R3, R2, R2", out intPart2);
-            Debug.Assert(intPart1 == 0 && intPart2 == 1);
+            NUnit.Framework.Assert.AreEqual(0, intPart1);
+            NUnit.Framework.Assert.AreEqual(1, intPart2);
 
             intPart1 = Day01Solution.GetDistanceFromStartingPoint("R1, R2, R2, R3, R3, R2, R5", out intPart2);
-            Debug.Assert(intPart1 == 4 && intPart2 == 2);
+            NUnit.Framework.Assert.AreEqual(4, intPart1);
+            NUnit.Framework.Assert.AreEqual(2, intPart2);
         }
     }
 }
